05fa23f72a4067b32cf05144ddcc1883398e9b20
Jon Boone <ipmonger@delamancha.org>
2021-01-17 15:13:58 -0500

    re-adds Creating-Your-First-C++-Program file

---
+++ 01-Your-First-C++-Program/Creating-Your-First-C++-Program.cpp
@@ -0,0 +1,10 @@
+// hello world program
+// main.cpp
+// From O'Reilly Learning Path Beginning C++ by Brian Foster
+// 2021-01-17
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
